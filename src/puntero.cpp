4d71514d31b44cf68c3b23d920bf5268c8c59da6
Jonathan Rodriguez <jrodriguez050796@gmail.com>
2020-06-08 19:35:04 -0600

    datos al garbage
    genera y enviar el id de los punteros

--- src/puntero.cpp
+++ src/puntero.cpp
@@ -1,14 +1,22 @@
 
 #include <iostream>
+#include </home/jonathan/Escritorio/Proyecto/Proyecto1_Datos2/src/singleton_Garbage_Collector.h>
+#include </home/jonathan/Escritorio/Proyecto/Proyecto1_Datos2/src/singleton_Garbage_Collector.cpp>
 using namespace std;
 class puntero {
 
 public:
+    
+    singleton_Garbage_Collector* garbage = &singleton_Garbage_Collector().Instancia();
     int *num;
     int data;
+    int Id;
     puntero& operator=(int x){
+        
+        
         this->data = x;
         this->num = &x;
+        this->Id = garbage->ID_Generar();
         return *this;
     }
     puntero* operator*(int x){
@@ -18,6 +26,8 @@ public:
     puntero& operator=(puntero y){
         this->data = y.data;
         this->num = y.num;
+        this->Id = y.Id;
+        garbage->sumarReferencia(y.Id);
         return *this;
     }
 
@@ -25,6 +35,8 @@ public:
 
 
         int *memoriaLiberada = x->num;
+        int idEliminar = x->Id;
+        garbage->Borrar_Direccion(idEliminar);
         delete x;
         return memoriaLiberada;
 
